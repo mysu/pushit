38fa0108d9492a1d23b3d6b51618a82b8620628f
emiguelt <edwin.triana@bizagi.com>
2016-09-08 14:34:18 -0500

    cljs from zeiste tutorial

--- pushit-server/src/clj/pushit_server/handler.clj
+++ pushit-server/src/clj/pushit_server/handler.clj
@@ -1,12 +1,12 @@
 (ns pushit-server.handler
   (:require [compojure.core :refer [GET defroutes]]
             [compojure.route :as route]
-            [ring.util.response :refer [resource-response response]]
+            [ring.util.response :refer [resource-response response content-type]]
             [ring.middleware.json :as middleware]
             [ring.middleware.defaults :refer [wrap-defaults api-defaults]]))
 
 (defroutes app-routes
-  (GET "/" [] (resource-response "index.html" {:root "public"}))
+  (GET "/" [] (content-type (resource-response "index.html" {:root "public"}) "text/html"))
   (GET "/widgets" [] (response [{:name "Widget 1"} {:name "Widget 2"}]))
   (route/resources "/")
   (route/not-found "Not Found"))
