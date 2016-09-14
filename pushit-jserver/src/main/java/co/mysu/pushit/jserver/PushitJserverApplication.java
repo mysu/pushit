dacc6005add0fc24fa865c68bf72214fa470baf3
emiguelt <edwin.triana@bizagi.com>
2016-09-14 18:26:07 -0500

    initial project for pushit-server java version

---
+++ pushit-jserver/src/main/java/co/mysu/pushit/jserver/PushitJserverApplication.java
@@ -0,0 +1,12 @@
+package co.mysu.pushit.jserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PushitJserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PushitJserverApplication.class, args);
+	}
+}
---
+++ pushit-jserver/src/main/java/co/mysu/pushit/jserver/ServletInitializer.java
@@ -0,0 +1,13 @@
+package co.mysu.pushit.jserver;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PushitJserverApplication.class);
+	}
+
+}
