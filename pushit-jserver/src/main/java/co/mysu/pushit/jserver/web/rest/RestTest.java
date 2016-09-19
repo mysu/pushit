ef4dd79f532b5da92ced4c83dc29c1ff7175697d
emiguelt <edwin.triana@bizagi.com>
2016-09-19 14:17:06 -0500

    adding index.html, removing unused dependencies

--- pushit-jserver/src/main/java/co/mysu/pushit/jserver/web/rest/RestTest.java
+++ pushit-jserver/src/main/java/co/mysu/pushit/jserver/web/rest/RestTest.java
@@ -1,6 +1,5 @@
 package co.mysu.pushit.jserver.web.rest;
 
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
---
+++ pushit-jserver/src/main/java/co/mysu/pushit/jserver/web/websocket/PushWSController.java
@@ -0,0 +1,8 @@
+package co.mysu.pushit.jserver.web.websocket;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class PushWSController {
+
+}
